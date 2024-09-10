d6c5af098a1d7224a73b7a9aceb1eee6c17a821f
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-10 09:26:53 -0300

    Adds custom person list in hello component.

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -2,10 +2,18 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "hello",
-  template: `
-    <h1>Hello World!</h1>
-  `,
+  templateUrl: "./hello.component.html",
 })
 export class HelloComponent {
+  name = "Fulano";
 
+  person1: Person = new Person("Fulano", 21);
+  person2: Person = new Person("Ciclano", 22);
+  person3: Person = new Person("Beltrano", 23);
+
+  persons: Person[] = [this.person1, this.person2, this.person3];
+}
+
+class Person {
+  constructor(public name: string, public age: number) {}
 }
